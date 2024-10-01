92eecb7a1aa10187a1a650d41af69be50986c297
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2024-10-01 20:35:11 +0530

    Add files via upload

---
+++ DAY 4-THREETOPICS.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		    int y=sc.nextInt();
+		        int z=sc.nextInt();
+		            int a=sc.nextInt();
+		            
+		            if(a==x||a==y||a==z){
+		                System.out.println("yes");
+		            }else{
+		                System.out.println("no");
+		            }
+
+	}
+}
