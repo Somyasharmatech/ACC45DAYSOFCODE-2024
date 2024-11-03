66031c1d4f8c6222bbf802c03f9f911b4f09c913
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2024-11-03 16:20:16 +0530

    Add files via upload

---
+++ DAY 32-TLG.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n =sc.nextInt();
+		int p1,p2,lead=0,win=0,diff,sum1=0,sum2=0;
+		while(n-->0){
+		    p1=sc.nextInt();
+		    p2=sc.nextInt();
+		    sum1+=p1;
+		    sum2+=p2;
+		    if(sum1>sum2)
+		    {
+		        diff=sum1-sum2;
+		        if(diff>lead){
+		            lead=diff;
+		            win=1;
+		        }
+		    }
+		else{
+		    diff=sum2-sum1;
+		    if(diff>lead){
+		        lead=diff;
+		        win=2;
+		    }
+		}
+
+	}
+	System.out.println(win+" "+lead);
+}
+}
